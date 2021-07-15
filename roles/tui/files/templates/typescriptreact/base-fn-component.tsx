db5ff7a26a61d9148a942d95d3746482b07abaf8
ziimir <ziimir9@gmail.com>
2021-07-15 11:33:00 +0300

    Update base-fn-component.tsx

--- roles/tui/files/templates/typescriptreact/base-fn-component.tsx
+++ roles/tui/files/templates/typescriptreact/base-fn-component.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, memo} from 'react';
 import {createCn} from 'bem-react-classname';
 
 import './{{_expr_:expand('%:t:r')}}.css';
@@ -6,7 +6,7 @@ import './{{_expr_:expand('%:t:r')}}.css';
 interface Props {
 }
 
-export const {{_expr_:substitute(expand('%:t:r'), '\(\%(\<\l\+\)\%(-\)\@=\)\|-\(\l\)', '\u\1\2', 'g')}}: FC<Props> = (props) => {
+export const {{_expr_:substitute(expand('%:t:r'), '\(\%(\<\l\+\)\%(-\)\@=\)\|-\(\l\)', '\u\1\2', 'g')}}: FC<Props> = memo((props) => {
     const cn = createCn('{{_expr_:expand('%:t:r')}}', props.className);
 
     return (
@@ -14,6 +14,6 @@ export const {{_expr_:substitute(expand('%:t:r'), '\(\%(\<\l\+\)\%(-\)\@=\)\|-\(
             {{_cursor_}}
         </div>
     );
-}
+});
 
 {{_expr_:substitute(expand('%:t:r'), '\(\%(\<\l\+\)\%(-\)\@=\)\|-\(\l\)', '\u\1\2', 'g')}}.displayName = '{{_expr_:substitute(expand('%:t:r'), '\(\%(\<\l\+\)\%(-\)\@=\)\|-\(\l\)', '\u\1\2', 'g')}}';
